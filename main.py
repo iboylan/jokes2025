b7636876e82889bc5f668b86db8bdcf154352a34
iboylan <pipiel326@gmail.com>
2026-02-03 18:16:28 +0000

    h

--- main.py
+++ main.py
@@ -4,63 +4,34 @@
 # make this performance task ready for submission
 # To give the user a fun experience hearing knock knock jokes
 
-# joke = input("Do you want to hear a joke? ")
-# if joke == "no":
-#     print("Okay suit yourself!")
-# while joke == "yes":
-#     print("Great, Let's Play")
-#     question = input("Do you want to hear a joke about robbers, tanks, or pencils? ")
-#     if question == "robbers":
-#         input("Knock Knock ")
-#         input("Calder")
-#         print("Calder police - I've been robbed!")
-#         joke = input("Do you want to hear another joke or are you finished? ")
-#     elif question == "tanks":
-#         input("Knock Knock ")
-#         input("Tank ")
-#         input("You are welcome! ")
-#         joke = input("Do you want to hear another joke or are you finished? ")
-#     elif question == "pencils":
-#         input("Knock Knock ")
-#         input("Broken pencil ")
-#         input("Nevermind, it's pointless! ")
-#         joke = input("Do you want to hear another joke or are you finished? ")
-# if joke == "finished":
-#     rate = int(input("Please rate our game 1-10! "))
-#     final_score = int(rate * 10)
-#     print(str(final_score) + " percent satisfaction rate")
-#     friend = input("Would you recommend this game to a friend? ")
-
-#     if friend == "yes" or friend == "maybe":
-#         print("Thanks, we appreciate it. ")
-#     else:
-#         print("Sorry you did not enjoy it. ")
-
 yes = ["Yes", "yes", "y", "Y", "YES"]
 no = ["No", "no", "n", "N", "NO"]
+
 def jokes(joke):
     namelist = ["robbers", "tanks", "pencils"]
+    joke = joke.lower()
+    found = False
+
     for name in namelist:
-        if name == joke.lower():
+        if name == joke:
+            found = True
             if name == "robbers":
                 input("Knock knock! ")
                 input("Calder ")
                 input("Calder police - I've been robbed! ")
-                return
-            if name == "tanks":
+            elif name == "tanks":
                 input("Knock knock! ")
                 input("Tanks ")
-                input("You are welcome! ")
-                return
-            if name == "pencils":
-                input("Knock Knock! ")
+                input("You're welcome! ")
+            elif name == "pencils":
+                input("Knock knock! ")
                 input("Broken pencil ")
                 input("Nevermind, it's pointless! ")
-                return
-        elif name != joke.lower():
-            print("not an option")
-            return
+            break
 
+    if not found:
+        print("Not an option.")
+        
 def newjoke():
     newjokelist = []
     newjokelist.append(input("What's your joke about?: "))
